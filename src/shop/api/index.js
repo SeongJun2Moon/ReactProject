bd47cb49a31cf24b9a651141b93ad4485108f58c
SJ <sjmun0420@gmail.com>
2022-12-14 15:54:40 +0900

    fdsa

--- src/shop/api/index.js
+++ src/shop/api/index.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 const server = `http://localhost:8000`
-export const irisApi = req => axios.get(`${server}/shop/iris/iris`, req)
+export const irisApi = req => axios.post(`${server}/shop/iris/iris`, req)
--- src/shop/components/Iris.jsx
+++ src/shop/components/Iris.jsx
@@ -1,25 +1,42 @@
 import { irisApi } from '../api'
+import {useState} from 'react'
 
 
 const Iris = () => {
+    const [inputs, setInputs] = useState({})
+    const {SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm} = inputs;
     
-    const onClick = () => {
-        irisApi().then(()=>{
-            alert("됐어유")
+    const onChange = e => {
+        e.preventDefault()
+        const {value, name} = e.target 
+        setInputs({...inputs, [name]: value})
+    }
+    
+    const onClick = e =>{
+        e.preventDefault()
+        const IrisRequests = {SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm}
+        alert(`사용자 이름: ${JSON.stringify(IrisRequests)}`)
+        irisApi(IrisRequests)
+        .then((res)=>{
+            console.log(`Response is ${res.config.data}`)
+            localStorage.setItem('token', JSON.stringify(res.config.data))
+            alert(`찾는 품종 : ${JSON.stringify(res.data.붓꽃품종)}`)
         })
-        .catch(()=>{
-            alert("안됐어유")
+        .catch((err)=>{
+            console.log(err)
+            alert('땡')
         })
     }
     
     return (
-        <>
-            <button onClick={()=>onClick()}>붓꽃 분류</button>
-        </>
-    )
-    
-    
-}
+    <>
+        꽃잎 길이 : <input type="text" name="PetalLengthCm" onChange={onChange} /><br/>
+        꽃잎 폭: <input type="text" name="PetalWidthCm" onChange={onChange} /><br/>
+        꽃받침 길이: <input type="text" name="SepalLengthCm" onChange={onChange} /><br/>
+        꽃받침 폭: <input type="text" name="SepalWidthCm" onChange={onChange} /><br/>
+        <button onClick={onClick}> 실행 </button>
+    </>
+    )}
 
-
+    
 export default Iris;
